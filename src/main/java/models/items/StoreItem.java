e280c10b9835671c5c7e15895d10d2be6e52c5b2
Matt Myers <myersmatt1987@gmail.com>
2022-08-22 09:48:21 -0700

    Added annotations to StoreItem.java

--- src/main/java/models/items/StoreItem.java
+++ src/main/java/models/items/StoreItem.java
@@ -1,22 +1,34 @@
 package models.items;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="store_item")
 public class StoreItem {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "item_id")
 	private int itemId;
+	@Column(name = "item_name")
 	private String itemName;
+	@Column(name="item_prioce")
 	private double itemPrice;
+	@Column(name="item_quantity")
 	private int itemQuantity;
-
+	@Column(name="description")
+	private String description;
 	public StoreItem() {
-		this(0, "itemName", 0.00, 0);
+		this(0, "itemName", 0.00, 0, "This is an item");
 		
 	}
 	
-	public StoreItem(int itemId, String itemName, double itemPrice, int itemQuantity) {
+	public StoreItem(int itemId, String itemName, double itemPrice, int itemQuantity, String description) {
 		this.itemId = itemId;
 		this.itemName = itemName;
 		this.itemPrice = itemPrice;
 		this.itemQuantity = itemQuantity;
+		this.description = description;
 	}
 	
 	public int getitemId() {
