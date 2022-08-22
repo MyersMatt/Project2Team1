a27a41e605359dec5b1b1e8c7e6ea4a73d19c79f
Matt Myers <myersmatt1987@gmail.com>
2022-08-22 13:25:24 -0700

    Added additional test cases to driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,18 +8,39 @@ import java.util.Optional;
 
 public class Driver {
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-        Session session = HibernateUtil.getInstance().openSession();
         RegistrationService registrationService = new RegistrationService();
         Optional<User> user = registrationService.register(
-                "jon",
-                "doe",
-                "jdoe@example.com",
-                "jdoe",
+                "Steve",
+                "Rogers",
+                "srogers@example.com",
+                "srogers",
                 "password",
                 "123 Example St.",
                 "1111-1111-1111"
         );
-        if(user.isPresent()) System.out.println(user.get());
+        user.ifPresent(System.out::println);
+
+        user = registrationService.register(
+                "Bruce",
+                "Banner",
+                "bbanner@example.com",
+                "bbanner",
+                "password",
+                "123 Example St.",
+                "1111-1111-1111"
+        );
+        user.ifPresent(System.out::println);
+
+        user = registrationService.register(
+                "Tony",
+                "Stark",
+                "tstark@example.com",
+                "tstark",
+                "password",
+                "123 Example St.",
+                "1111-1111-1111"
+        );
+        user.ifPresent(System.out::println);
     }
 }
+
