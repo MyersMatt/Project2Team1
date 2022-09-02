9b9fe8e75f57a6cd119500917212e589d1329747
James Megargel <megajam750@Jamess-MacBook-Air.local>
2022-09-01 18:42:43 -0700

    finished enabling buy-btn; restored full functionality to shopping cart

--- src/main/webapp/resources/js/products.js
+++ src/main/webapp/resources/js/products.js
@@ -138,6 +138,46 @@ function init() {
         console.log( err.message );
     })
 
+    container = document.getElementById('prod-list');
+
+    fetch(req)
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP');
+        }
+    })
+    .then((json)=>{
+        const length = Object.keys(json).length;
+            for (var i = 0; i < length; i++) {
+            let art = document.createElement('article');
+            art.classList.add('prod-row')
+            art.setAttribute('id', json[i].itemId);
+            let prod_row_contents =`            
+            <img class="prod-image" src="${json[i].imageUrl}" alt="image">
+            <div class="prod-text">
+                <h2 class="prod-title">${json[i].itemName}</h2>
+                <p class="prod-desc">${json[i].description}</p>
+            </div>
+            <div class="prod-numb">
+                <p class="prod-price">${json[i].itemPrice}</p>
+                <p class="prod-quantity"><strong>${json[i].itemQuantity}</strong><br><em>in stock</em></p>
+                <button class="buy-btn">Purchase</button>
+            </div>`
+            art.innerHTML = prod_row_contents;
+            container.append(art);
+        }
+        const addToCartButtons = document.getElementsByClassName('buy-btn');
+        for (var i = 0; i < addToCartButtons.length; i++) {
+            var button = addToCartButtons[i]
+            button.addEventListener('click', addToCartClicked)
+        }
+    })
+    .catch((err)=>{
+        console.log( err.message );
+    })
+
     let removeCartItemButtons = document.getElementsByClassName('btn-danger');
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         button = removeCartItemButtons[i];
