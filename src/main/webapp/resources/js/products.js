866b527dc6ac49bd71004d879ba751dff176702e
JD Megargel <109110533+MegaJam750@users.noreply.github.com>
2022-09-06 09:38:17 -0700

    Enable purchase btn (#27)
    * finished enabling buy-btn; restored full functionality to shopping cart
    
    * eliminated some residual html code and added comments
    
    * finished enabling buy-btn; restored full functionality to shopping cart
    
    * eliminated some residual html code and added comments
    
    * Create order history fxnality (#26)
    
    * decomposing Purchase function; intermediate save
    
    * accessed prod-id
    
    * created itemsPurchased array
    
    * created itemsPurchased array that can be persisted to either localStorage or our order history database
    
    * updating database with realistic product images
    
    * finished enabling buy-btn; restored full functionality to shopping cart
    
    * eliminated some residual html code and added comments
    
    * decomposing Purchase function; intermediate save
    
    * accessed prod-id
    
    * created itemsPurchased array
    
    * created itemsPurchased array that can be persisted to either localStorage or our order history database
    
    * updating database with realistic product images
    
    * preparing the look
    
    * updating work
    
    * decomposing Purchase function; intermediate save
    
    * accessed prod-id
    
    * created itemsPurchased array
    
    * created itemsPurchased array that can be persisted to either localStorage or our order history database
    
    * updating database with realistic product images
    
    * preparing the look
    
    * updating work
    
    * updating images, logos, and products page fxnality
    
    Co-authored-by: James Megargel <megajam750@Jamess-MacBook-Air.local>
    
    Co-authored-by: James Megargel <megajam750@Jamess-MacBook-Air.local>

--- src/main/webapp/resources/js/products.js
+++ src/main/webapp/resources/js/products.js
@@ -1,11 +1,60 @@
+//Dynamic Display Products List
 
+//set variables needed for GET fetchAPI
+let uri = 'http://localhost:8080/Project2/api/items/getAllItems';
+let req = new Request(uri, {method: 'GET'});
+let container;
+
+//make sure products.html page is ready to be modified
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+    document.addEventListener('DOMContentLoaded', init)
 } else {
-    ready()
+    init()
 }
 
-function ready() {  
+//initialization functions to setup products page
+function init() {  
+
+    container = document.getElementById('prod-list');
+
+    //here's the fetch for our store-items
+    fetch(req)
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            throw new Error('BAD HTTP');
+        }
+    })
+    .then((json)=>{
+        const length = Object.keys(json).length;        //will pull all objects from the database
+            for (var i = 0; i < length; i++) {
+            let art = document.createElement('article');
+            art.classList.add('prod-row')               //each prod-row is a html <article>
+            art.setAttribute('id', json[i].itemId);     //'prod-id-'+  <-- place before json[i]
+            let prod_row_contents =`                    
+            <img class="prod-image" src="${json[i].imageUrl}" alt="image">
+            <div class="prod-text">
+                <h2 class="prod-title">${json[i].itemName}</h2>
+                <p class="prod-desc">${json[i].description}</p>
+            </div>
+            <div class="prod-numb">
+                <p class="prod-price">${json[i].itemPrice}</p>
+                <p class="prod-quantity"><strong>${json[i].itemQuantity}</strong><br><em>in stock</em></p>
+                <button class="buy-btn">Purchase</button>
+            </div>`
+            art.innerHTML = prod_row_contents;
+            container.append(art);
+        }
+        const addToCartButtons = document.getElementsByClassName('buy-btn');
+        for (var i = 0; i < addToCartButtons.length; i++) {
+            var button = addToCartButtons[i]
+            button.addEventListener('click', addToCartClicked)
+        }
+    })
+    .catch((err)=>{
+        console.log( err.message );
+    })
 
     let removeCartItemButtons = document.getElementsByClassName('btn-danger');
     for (var i = 0; i < removeCartItemButtons.length; i++) {
@@ -19,53 +68,10 @@ function ready() {
         input.addEventListener('change', quantityChanged)
     }
 
-    var addToCartButtons = document.getElementsByClassName('buy-btn')
-    for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
-    }
-
-    // var addToCartButtons = document.querySelector('buy-btn');
-    // var 
-
-
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 
 
-
-}
-
-
-
-
-
-function purchaseClicked() {
-
-    //is user registered or a guest:  check
-
-        //if registered, create order history
-
-
-        //if not, do something else (or not)
-
-
-    //need to update item quantity in item
-
-
-    //alerting about the purchase
-    alert("Thank you for your purchase")
-
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-
-    //removing the items 
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()   //makes total zero
-}
-
-
-
+}   //end of initialization function
 
 
 function removeCartItem(event) {
@@ -77,6 +83,7 @@ function removeCartItem(event) {
 
 function quantityChanged(event) {
     var input = event.target
+    // let pRow = input.parentElement.parentElement.
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
@@ -85,17 +92,19 @@ function quantityChanged(event) {
 
 
 function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
+    // console.log('button clicked')
+    var button = event.target       //target button is buy-btn
+    var shopItem = button.parentElement.parentElement       //parent's parent = article with class prod-row
+    var pid = shopItem.getAttribute('id');
     var title = shopItem.getElementsByClassName('prod-title')[0].innerText
     var price = shopItem.getElementsByClassName('prod-price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('prod-image')[0].src
-    addItemToCart(title, price, imageSrc)
+    addItemToCart(pid, title, price, imageSrc)       //take the prod-item, send to cart, make it a cart-item
     updateCartTotal()
 }
 
 
-function addItemToCart(title, price, imageSrc) {
+function addItemToCart(pid, title, price, imageSrc) {        //receives prod-item and makes it a cart-item
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -107,7 +116,7 @@ function addItemToCart(title, price, imageSrc) {
         }
     }
     var cartRowContents = `
-    <div class="cart-item cart-column">
+    <div class="cart-item cart-column bought-item" id="${pid}">
         <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
         <span class="cart-item-title">${title}</span>
     </div>
@@ -138,4 +147,26 @@ function updateCartTotal() {
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
+
+function purchaseClicked() {
+
+    var itemsPurchased = [];                                                        //declares the order history array
+    var boughtItem = document.getElementsByClassName('bought-item')                 //item number
+    var boughtItemQty = document.getElementsByClassName('cart-quantity-input')      //quantity bought
+    for (var i = 0; i < boughtItem.length; i++) {
+        itemsPurchased[boughtItem[i].getAttribute('id')] = boughtItemQty[i].value   //loads values into the order history array
+        }
+    console.log(itemsPurchased)
+
+        //////// original content ///////////
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    //removing the items 
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    alert(`Thank you for your purchase of items`)
+    updateCartTotal()   //makes total zero
+    //////// original content ///////////
 }
